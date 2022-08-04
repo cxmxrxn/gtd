53ef5f84841e1b01163b271478c53c484a3706fb
Your Name <you@example.com>
2022-08-04 19:01:12 -0400

    routes and controllers

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -4,6 +4,9 @@ const mongoEndpoint = 'mongodb://localhost/';
 const dbName = 'gtddb';
 
 mongoose
-    .connect(mongoEndpoint + dbName)
+    .connect(mongoEndpoint + dbName, {
+        useNewUrlParser: true,
+        // useUnifiedTypology: true,
+    })
     .then(() => console.log(`connected to ${dbName} db`))
     .catch((err) => console.log(`ERROR when connecting to ${dbName} db`, err));
--- server/server.js
+++ server/server.js
@@ -1,15 +1,17 @@
-require('../server/config/mongoose.config');
 const express = require('express');
 const app = express();
 const PORT = 8000;
 const cors = require('cors');
 
 app.use(express.json());
+app.use(express.urlencoded({extended: true }));
 // app.use(cors());
 app.use(cors({origin:'http://localhost:3000'}));
 
-require('./routes/project.routes')(app);
-require('./routes/action.routes')(app);
-require('./routes/context.routes')(app);
+
+require('../server/config/mongoose.config');
+// require('./routes/project.routes')(app);
+// require('./routes/action.routes')(app);
+// require('./routes/context.routes')(app);
 
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
